966ab1e9f91f2efb2caca0b395888791b92e4f3e
Sedemsenaya <Sedemsenaya.25@gmail.com>
2026-01-22 10:13:01 -0800

    first commit of backend with videos in gitignore

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+// const mysql = require("mysql2/promise");
+const fs = require("fs");
+const problems = JSON.parse(fs.readFileSync("./leetnote.json", "utf8"));
+const cors = require("cors");
+const path = require("path");
+
+
+const app = express();
+app.use(express.json());
+
+// ✅ CORS (allow React dev server)
+// app.use(cors({
+//     // origin: "http://localhost:5173"
+//     origin: "https://leetnote-backend-separate.onrender.com"
+// }));
+
+app.use(cors());
+
+// 🔥 LOG EVERY REQUEST
+app.use((req, res, next) => {
+    console.log("➡️ Incoming:", req.method, req.url);
+    next();
+});
+
+// // MySQL pool
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "Seditech@25",
+//     database: "leetnotedb"
+// });
+
+// SQLite database (local file)
+// const db = new Database(path.join(__dirname, "leetnote.db"));
+
+
+
+// Serve the "videos" folder statically
+app.use('/videos', express.static(path.join(__dirname, 'videos')));
+
+// Root
+app.get("/", (req, res) => {
+    res.send("Backend is alive 🚀");
+});
+
+// // Problems
+// app.get("/problems", async (req, res) => {
+//     console.log("🔥 /problems HANDLER reached");
+//     const [rows] = await db.query(
+//         "SELECT id, Problem, Pattern, Note, Visualization, Difficulty FROM leetnote"
+//     );
+//     res.json(rows);
+// });
+
+// app.get("/problems", (req, res) => {
+//     console.log("🔥 /problems HANDLER reached");
+//
+//     db.all(
+//         "SELECT id, Problem, Pattern, Note, Visualization, Difficulty FROM leetnote",
+//         (err, rows) => {
+//             if (err) {
+//                 console.error("DB ERROR:", err);
+//                 return res.status(500).json({ error: "Database error" });
+//             }
+//             res.json(rows);
+//         }
+//     );
+// });
+
+
+app.get("/problems", (req, res) => {
+    res.json(problems);
+});
+
+// Render requires dynamic port
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
+app.get("/debug-videos-path", (req, res) => {
+    res.send(path.join(__dirname, "videos"));
+});
